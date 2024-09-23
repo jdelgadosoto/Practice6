d1ef9191860dfa7468fa52103ef1933ef50321f5
jdelgadosoto <jdelgadosoto@deloitte.com>
2024-09-23 11:41:29 -0600

    Add files via upload

---
+++ ProductControllerTest.cs
@@ -0,0 +1,24 @@
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Controllers;
+using Moq;
+
+namespace ProductsTest
+{
+    public class ProductControllerTest
+    {
+        private readonly ProductController _controller;
+        private readonly Mock<>
+        public ProductControllerTest()
+        {
+            _controller = new ProductController();
+        }
+        
+            
+            
+    }
+
+
+}
+
+
